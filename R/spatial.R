ed926c4403b371a576a8c9a99a112bd4cc1963c3
sviatkh <sviatoslavkharuk@gmail.com>
2026-04-22 16:12:31 +0200

    add nimbus prediction function

--- R/spatial.R
+++ R/spatial.R
@@ -40,19 +40,104 @@ Run_novae <- function(adata_path = NULL, accelerator = "cuda") {
   }, adata_path = adata_path, accelerator = accelerator)
 }
 
-#' Run_nimbus
 #' 
-#' This functions runs Nimbus.
+#' Run_nimbus # based on Nimbus-Inference example notebook https://github.com/angelolab/Nimbus-Inference/blob/main/templates/1_Nimbus_Predict.ipynb 
 #' 
 #' @export
+
 Run_nimbus <- function() {
   proc <- basilisk::basiliskStart(.nimbus)
   on.exit(basilisk::basiliskStop(proc))
   basilisk::basiliskRun(proc, function() {
-    sg <- reticulate::import("nimbus_inference")
-    message("nimbus_inference was loaded!")
-    return(TRUE)
-  })
-}
-  
-  
+
+  # import libraries
+    warnings <- import("warnings")
+    warnings$simplefilter("ignore")
+    os <- import("os")
+    nimbus_mod <- import("nimbus_inference.nimbus")
+    Nimbus <- nimbus_mod$Nimbus
+    prep_naming_convention <- nimbus_mod$prep_naming_convention
+    utils_mod <- import("nimbus_inference.utils")
+    MultiplexDataset <- utils_mod$MultiplexDataset
+    io_utils <- import("alpineer.io_utils")
+    nimbus_inference <- import("nimbus_inference")
+    example_dataset <- nimbus_inference$example_dataset
+    viewer_mod <- import("nimbus_inference.viewer_widget")
+    NimbusViewer <- viewer_mod$NimbusViewer
+
+    # set up base directory
+    base_dir <- os$path$normpath(base_dir)
+
+    # download example dataset
+    example_dataset$get_example_dataset(dataset="cluster_pixels", save_dir=base_dir, overwrite_existing=FALSE)
+
+    # set up file paths
+    tiff_dir <- os$path$join(base_dir, "image_data")
+    deepcell_output_dir <- os$path$join(base_dir, "segmentation", "deepcell_output")
+    nimbus_output_dir <- os$path$join(base_dir, "nimbus_output")
+    os$makedirs(nimbus_output_dir, exist_ok=TRUE)
+    io_utils$validate_paths(list(base_dir, tiff_dir, deepcell_output_dir, nimbus_output_dir))
+
+    # channels to include
+    include_channels <- c(
+      "CD3", "CD4", "CD8", "CD14", "CD20", "CD31", "CD45", "CD68", "CD163", "CK17", "Collagen1",
+      "ECAD", "Fibronectin", "GLUT1", "HLADR", "IDO", "Ki67", "PD1", "SMA", "Vim"
+    )
+
+    # get FOV names and paths
+    fov_names <- os$listdir(tiff_dir)
+    fov_names <- Filter(function(x) !startsWith(x, "."), fov_names)
+    fov_paths <- lapply(fov_names, function(f) os$path$join(tiff_dir, f))
+
+    # prepare segmentation naming convention
+    segmentation_naming_convention <- prep_naming_convention(deepcell_output_dir)
+
+    # test segmentation naming convention
+    if (os$path$exists(segmentation_naming_convention(fov_paths[[1]]))) {
+      message("Segmentation data exists for fov 0 and naming convention is correct")
+    } else {
+      message("Segmentation data does not exist for fov 0 or naming convention is incorrect")
+    }
+
+    # create dataset
+    dataset <- MultiplexDataset(
+      fov_paths = fov_paths,
+      suffix = ".tiff",
+      include_channels = include_channels,
+      segmentation_naming_convention = segmentation_naming_convention,
+    output_dir = nimbus_output_dir
+    )
+
+    # init Nimbus
+    nimbus <- Nimbus(
+      dataset = dataset,
+      save_predictions = TRUE,
+      batch_size = 4,
+      test_time_aug = TRUE,
+      input_shape = as.integer(c(1024, 1024)),
+      device = "auto",
+      output_dir = nimbus_output_dir,
+      compile_model = FALSE,
+      mixed_precision = FALSE
+    )
+
+    # check inputs
+    nimbus$check_inputs()
+
+    # Prepare normalization dictionary
+    dataset$prepare_normalization_dict(
+      quantile = 0.999,
+      n_subset = 20L,
+      clip_values = tuple(0L, 2L),
+      multiprocessing = TRUE,
+      overwrite = TRUE
+    )
+
+    # make predictions
+    cell_table <- nimbus$predict_fovs()
+    return(cell_table)
+    })
+  }
+
+
+
